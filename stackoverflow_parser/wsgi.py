94a99402bfcb3c0caa067e71a6f0306dd7b7b30b
Dmitry <vip.shoroch@gmail.com>
2018-04-22 14:24:38 +0300

    project structure changing

--- stackoverflow_parser/wsgi.py
+++ stackoverflow_parser/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for stackoverflow_parser project.
+WSGI config for s project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stackoverflow_parser.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "s.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
